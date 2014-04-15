30bbdd608c6f89a82981f5cba43ea448ba2bb84e
youzipi <blake_12@qq.com>
2014-04-15 19:28:28 +0800

    哲学家吃面——实现 TBD: * 代码优化 * 链表修改 * 打印处理 * 逻辑调整 Signed-off-by: youzipi <blake_12@qq.com>

--- Philosophers-1/ph-1.cpp
+++ Philosophers-1/ph-1.cpp
@@ -12,10 +12,12 @@ typedef struct Pcb {
 	int num;      
 	int state;
 	struct Pcb *next;   //阻塞时的顺序
+	//int sum;
 }Pcb, *link;
 int fork[5] = { 1, 1, 1, 1, 1 };//餐叉；
 link ph;
-void philosopher(link ph);
+int sum;
+int philosopher(link ph);
 
 bool P(int &c1,int &c2) {
 	if (c1 && c2) {
@@ -34,7 +36,7 @@ void init(link &l) {
 	//for (int i = 0; i < NUM; i++) {
 		l = (link)malloc(sizeof(Pcb));//建立新的结点,并初始化为生产者
 		//ph->num = i + 1;
-		l->state = thinking;
+		l->state = hungry;
 		l->next = NULL;
 
 }
@@ -43,34 +45,53 @@ void think(int i) {
 }
 void eat() {
 	link p = ph;
-	while (p->next != NULL) {
-		if (p->next->state == eating) {
-			cout << p->next->num << "号哲学家正在进餐。。。" << endl;
-			V(fork[(p->next->num + 1) % 5], fork[p->next->num]);
-			p->next = p->next->next;
+	while (p != NULL) {
+	//	if (p->next->state == eating) {
+	//		cout << p->next->num << "号哲学家正在进餐。。。" << endl;
+	//		V(fork[(p->next->num + 1) % 5], fork[p->next->num]);
+	//		p->next = p->next->next;
+	//	}
+	//	else
+	//		cout << p->next->num << "号哲学家在等待餐叉。。。" << endl;
+	//}
+		if (p->state == eating) {
+			cout <<"\t"<< p->num << "号哲学家吃完了。。。" << endl;
+			V(fork[p->num - 1], fork[p->num % 5]);
+			p->state = thinking;
+			sum--;
 		}
-		else
-			cout << p->next->num << "号哲学家在等待餐叉。。。" << endl;
+		else if (p->state == hungry)
+			cout << p->num << "号哲学家在等待餐叉。。。" << endl;
+		p = p->next;
+
 	}
 	cout << "---------------------" << endl;
+	_sleep(500);
 	philosopher(ph);
-
 }
 
 void getfork(int i, int t) {
-	cout << t << "号哲学家获得餐叉" << t << " " << (t+1) % 5 << endl;
+
+	cout << t << "号哲学家获得餐叉" << t << " " << (t == 5?1:t+1) << endl;
 }
 
-void philosopher(link ph) {
+int philosopher(link ph) {
 	bool flag;
 	link p = ph;
-	if (p->next == NULL)
-		return;
-	while (p->next != NULL) {
+	if (p == NULL)
+		return -1;
+	if (sum == 0)
+		return 0;
+	while (p != NULL ) {
+		if (p->state == thinking) {
+			p = p->next;
+			continue;
+		}
+
 		int i = p->num;
 		//think(i);
 		//int t = (i + 1) % NUM;
-		flag = P(fork[i-1], fork[i]);
+		flag = P(fork[i-1], fork[i % 5]);
 		if (flag) {
 			p->state = eating;
 			getfork(i-1, i);
@@ -80,50 +101,73 @@ void philosopher(link ph) {
 		//V(fork[i - 1], fork[i]);
 	}
 	eat();
+	return 0;
 }
 
 int main() {
 	//time_t t;
 	//int random;
 	init(ph);
+	//init(head);
+	//ph = head->next;
 	//int i;
 	//bool flag;
-	int hungry[5];
-	int temp,random;
-	int j;
+	int hungry[5] = {1,2,3,4,5};
+	int a[5];
+	//int temp,random;
+	int rd, random;
+	int i,j;
 	link p,p1;
 	while (1) {
 		time_t t;
 		srand((unsigned)time(&t));
 		random = rand() % 5 + 1;//随机函数产生随机数 
 		cout << random << "位哲学家饿了" << endl;
-		for (int h = 0; h < random; h++)
+		/*for (int h = 0; h < random; h++)
 			hungry[h] = 0;
 		for (int i = 0; i < random; i++) {
 			srand((unsigned)time(NULL));
 			temp = rand() % random + 1;
 			for (j = 0; j < i; j++) {
-				if (hungry[j] == temp) {
-					i--;
-					break;
-				}
+			if (hungry[j] == temp) {
+			i--;
+			break;
+			}
 			}
 			if (j == i)
-				hungry[j] = temp;
+			hungry[j] = temp;
+			}
+			*/
+		rd = random;
+		j = 0;
+		while (rd) {
+			time_t t;
+			srand((unsigned)time(&t));
+			i = rand() % rd;
+			a[j] = hungry[i];
+			j++;
+			hungry[i] = hungry[rd - 1];
+			rd--;
+			hungry[rd] = rd + 1;
 		}
 		for (int i = 0; i < random; i++)
-			cout << hungry[i] << "\t";
+			cout << a[i] << "\t";
 		cout << endl;
+
 		p1 = p = ph;
+		sum = 0;
 		for (int i2 = 0; i2 < random; i2++) {
-			p->num = hungry[i2];
+			p->num = a[i2];
+			p->state = 1;
+			sum++;
+			if ((i2 + 1) == random)
+				break;
 			p1 = p;
 			init(p->next);
 			p = p->next;
-
 		}
 		philosopher(ph);
-		_sleep(500);
+		_sleep(1500);
 	}
 	return 0;
 }
